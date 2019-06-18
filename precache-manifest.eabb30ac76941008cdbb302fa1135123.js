be200ecbd1f203684a5168fbdf91efd26978afe3
CastelloDev <castello.dev@gmail.com>
2019-06-19 00:01:07 +0200

    website init

---
+++ precache-manifest.eabb30ac76941008cdbb302fa1135123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c196396d9730615d07c2e6291a3b575a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ff78ed7896f4e0379d8",
+    "url": "/static/css/main.0d7ed9ef.chunk.css"
+  },
+  {
+    "revision": "c3e7ecb92e80971b3a1b",
+    "url": "/static/js/2.765862ca.chunk.js"
+  },
+  {
+    "revision": "0ff78ed7896f4e0379d8",
+    "url": "/static/js/main.e06df455.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fffe32dbd3e8646bb501c96e6e7ee2d9",
+    "url": "/static/media/logo.fffe32db.svg"
+  }
+]);
